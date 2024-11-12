3e61f2ac23385f0b0d54d000da49d847f5c1e852
spaykel <spaykel007@gmail.com>
2024-11-12 22:33:47 +0100

    login

--- src/main/java/com/wtm/spring_boot_wtm/controller/UserController.java
+++ src/main/java/com/wtm/spring_boot_wtm/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.wtm.spring_boot_wtm.controller;
 
 import com.wtm.spring_boot_wtm.model.User;
+import com.wtm.spring_boot_wtm.model.ResponseMessage;
+import com.wtm.spring_boot_wtm.model.LoginResponse;
 import com.wtm.spring_boot_wtm.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,9 +18,9 @@ public class UserController {
 
     // Register new user
     @PostMapping
-    public ResponseEntity<?> saveUser(@RequestBody User user){
+    public ResponseEntity<?> saveUser(@RequestBody User user) {
         System.out.println(user.toString());
-        if (userService.findByUsername(user.getUsername()) != null){
+        if (userService.findByUsername(user.getUsername()) != null) {
             return new ResponseEntity<>(HttpStatus.CONFLICT); // Username already exists
         }
         return new ResponseEntity<>(userService.saveUser(user), HttpStatus.CREATED); // User saved
@@ -29,14 +31,15 @@ public class UserController {
     public ResponseEntity<?> login(@RequestBody User user) {
         User existingUser = userService.findByUsername(user.getUsername());
         if (existingUser == null) {
-            return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(new ResponseMessage("User not found"), HttpStatus.NOT_FOUND);
         }
 
         // Compare password (in plain text for now)
         if (existingUser.getPassword().equals(user.getPassword())) {
-            return new ResponseEntity<>("Login successful", HttpStatus.OK);
+            // Return user details without the token
+            return new ResponseEntity<>(new LoginResponse(existingUser.getId(), existingUser.getUsername()), HttpStatus.OK);
         } else {
-            return new ResponseEntity<>("Invalid credentials", HttpStatus.UNAUTHORIZED);
+            return new ResponseEntity<>(new ResponseMessage("Invalid credentials"), HttpStatus.UNAUTHORIZED);
         }
     }
 }

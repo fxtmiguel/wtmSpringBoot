1fe2bd52f7698c97830553d39ccc6198af44efa5
TylerHamilton03 <thamil02@calpoly.edu>
2024-11-26 19:39:28 +0100

    removed an unused import

--- src/test/java/com/wtm/spring_boot_wtm/ReviewControllerTest.java
+++ src/test/java/com/wtm/spring_boot_wtm/ReviewControllerTest.java
@@ -15,7 +15,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
